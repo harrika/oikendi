aaec27af9c8741570c9a4f5a9fe19652b3f4f788
henry kirya <henry.kirya@gmail.com>
2019-08-23 18:02:38 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = "kirya o'tentena";
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>weather app</h1>
+<hr>
+
+<div class="field">
+  <p class="control has-icons-left has-icons-right">
+   <span class="icon is-small is-left">
+   <i class="fas fa-cloud-rain" style="color:hsl(141, 71%, 71%)"></i>
+   </span>
+   <input class="input" type="text" placeholder="Enter City name">
+  </p>
+</div>
