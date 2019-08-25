42732af43ede87feb3809889214ae5e468df3717
henry kirya <henry.kirya@gmail.com>
2019-08-25 10:24:14 +0300

    complete one

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,99 @@
 <script>
-	let name = "kirya o'tentena";
+	let city = null;
+	let cntry = ""
+	let weatherloc = "";
+	let weatherdesc2 = "";
+	let weatherwind = "";
+	let weathertemp = null;
+	$: tempc = (weathertemp - 273.15).toFixed(2);; 
+	$: tempfar = ((tempc*9/5)+32).toFixed(2);	
+	
+function kendi() {
+	// city = mm;
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=a98fa2f15f9189fb40a7332d4855fa41`, {mode: 'cors'})
+	      .then(function(response){
+	        return response.json()
+	      })
+	      .then((response) => {
+	        weatherloc = response.name;
+	        cntry = response.sys.country;
+	        weathertemp = response.main.temp;
+	        weatherdesc2 = response.weather[0].description;
+	        weatherwind = response.wind.speed;
+	        console.log(response);
+	      })
+ }
+
 </script>
 
 <style>
-	h1 {
-		color: purple;
+	.mt {
+		margin-top: 20px;
+	}
+	.mb {
+		margin-bottom: 20px;
+	}
+	.lm {
+		margin-left: 20px;
 	}
 </style>
+	<section class="hero is-primary mt mb">
+	  <div class="hero-body">
+	    <div class="container">
+	      <h1 class="title">
+	        svelther
+	      </h1>
+	      <h2 class="subtitle">
+	        Know you weather!
+	      </h2>
+	    </div>
+	  </div>
+	</section>
 
-<h1>weather app</h1>
-<hr>
+<div class="container">
+	<div class="columns">
+		<div class="column is-4">
+				<div class="form">
+			 	<div class="field">
+				<label for="" class="label">
+				<i class="fas fa-cloud-rain" style="color:hsl(141, 71%, 71%)"></i> Enter a city name</label>
+			   <span>
+			   <input class="input" type="text" placeholder="city: London,uk" bind:value={city}>			   	
+			   </span>
 
-<div class="field">
-  <p class="control has-icons-left has-icons-right">
-   <span class="icon is-small is-left">
-   <i class="fas fa-cloud-rain" style="color:hsl(141, 71%, 71%)"></i>
-   </span>
-   <input class="input" type="text" placeholder="Enter City name">
-  </p>
-</div>
+				</div> <!-- end field -->
+
+				<div class="field">
+					<p class="control">
+					<button class="button is-info" on:click={()=>kendi()}>
+					check weather</button>			
+					</p>
+				</div>		
+				</div><!--  end form -->			
+		</div> <!-- end col1 -->
+
+		<div class="column is-8">
+		<table class="table is-narrow is-hoverable is-striped lm" style="width:50%">		
+			<tbody>
+				<tr>
+				  <td>location: </td> 
+				  <td>{weatherloc} {cntry}</td>
+				</tr>			  
+				<tr>
+				  <td>wind speed</td>
+				  <td>{weatherwind}</td>
+				</tr>			  
+				<tr>
+				  <td>temperatures:</td>
+				  <td>{tempc} oC, {tempfar} oF</td>
+				</tr>			  
+				<tr>
+				  <td>weather: </td>
+				  <td>{weatherdesc2}</td>
+				</tr>			  				
+			</tbody>			  
+		</table>
+		</div> <!-- end col2 -->
+	</div>	<!-- end cols -->
+	
+</div> <!-- end container -->
